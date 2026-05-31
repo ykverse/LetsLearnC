83a42653a46117d1c3ccd44b54a6dd3bf41c841e
yedhu <yedhukrishnaam@gmail.com>
2026-05-31 15:10:36 +0530

    base done

--- BaseRLE/rle.c
+++ BaseRLE/rle.c
@@ -3,23 +3,37 @@
 #include <string.h>
 
 
+void compress(){
+
+
+
+}
+
+void decompress(){
+
+
+    
+}
+
 int main(int argc, char *argv[])
 {
         if(argc != 2){
 
-            printf("Usage: %s <Compression | Decompression> <filename>" , argv[0]);
+            printf("Usage: %s <compress | decompress> <filename>" , argv[0]);
             exit(-1);
         }
 
         if (strcmp(argv[1] , "compress") == 0){
 
             printf("compressing....\n");
+            compres();
         }
 
         else if (strcmp(argv[1] , "decompress") == 0){
 
             printf("decompressing....\n");
-            
+            decompress();
+
         }
         else{
 
